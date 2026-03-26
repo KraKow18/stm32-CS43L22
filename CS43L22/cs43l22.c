8061b340bb605716c6bba1fcd0d39c3cd4296445
Mars <petitmonne39@hotmail.fr>
2026-03-26 21:28:55 +0100

    init

---
+++ CS43L22/cs43l22.c
@@ -0,0 +1,8 @@
+/*
+ * cs43l22.c
+ *
+ *  Created on: Mar 26, 2026
+ *      Author: mars
+ */
+
+
---
+++ CS43L22/cs43l22.h
@@ -0,0 +1,13 @@
+/*
+ * cs43l22.h
+ *
+ *  Created on: Mar 26, 2026
+ *      Author: mars
+ */
+
+#ifndef CS43L22_H_
+#define CS43L22_H_
+
+
+
+#endif /* CS43L22_H_ */
